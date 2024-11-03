0f27be06cd6defdf7876407a7b115ff6b9753dde
Barry Nolan <bazdagreat333@gmail.com>
2024-11-03 15:50:30 +0000

    Seeding & get endpoints

--- lib/ca1-stack.ts
+++ lib/ca1-stack.ts
@@ -1,16 +1,104 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as apig from "aws-cdk-lib/aws-apigateway";
+import * as cdk from "aws-cdk-lib";
+import * as lambdanode from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as custom from "aws-cdk-lib/custom-resources";
+import { Construct } from "constructs";
+import { generateBatch } from "../shared/util";
+import { albums } from "../seed/albums";
 
 export class Ca1Stack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here
+		//Table
+		const albumsTable = new dynamodb.Table(this, "AlbumsTable", {
+			billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+			partitionKey: {name: "id", type: dynamodb.AttributeType.NUMBER},
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
+			tableName: "Albums",
+		});
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Ca1Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+		//Seeding
+		new custom.AwsCustomResource(this, "albumsddbInitData", {
+			onCreate: {
+				service: "DynamoDB",
+				action: "batchWriteItem",
+				parameters: {
+					RequestItems: {
+						[albumsTable.tableName]: generateBatch(albums),
+					},
+				},
+				physicalResourceId: custom.PhysicalResourceId.of("albumsddbInitData")
+			},
+			policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
+				resources: [albumsTable.tableArn],
+			}),
+		});
+
+		//Functions
+		const getAllAlbumsFn = new lambdanode.NodejsFunction(
+			this,
+			"GetAllAlbumsFn",
+			{
+				architecture: lambda.Architecture.ARM_64,
+				runtime: lambda.Runtime.NODEJS_18_X,
+				entry: `${__dirname}/../lambda/getAllAlbums.ts`,
+				timeout: cdk.Duration.seconds(10),
+				memorySize: 128,
+				environment: {
+					TABLE_NAME: albumsTable.tableName,
+					REGION: 'eu-west-1',
+				},
+			}
+		);
+
+		const getAlbumByIdFn = new lambdanode.NodejsFunction(
+			this,
+			"GetAlbumByIdFn",
+			{
+				architecture: lambda.Architecture.ARM_64,
+				runtime: lambda.Runtime.NODEJS_18_X,
+				entry: `${__dirname}/../lambda/getAlbumById.ts`,
+				timeout: cdk.Duration.seconds(10),
+				memorySize: 128,
+				environment: {
+					TABLE_NAME: albumsTable.tableName,
+					REGION: 'eu-west-1',
+				},
+			}
+		);
+
+		//Permissions
+		albumsTable.grantReadData(getAllAlbumsFn)
+		albumsTable.grantReadData(getAlbumByIdFn)
+
+		//REST API setup
+		const api = new apig.RestApi(this, "RestAPI", {
+			description: "CA1 API",
+			deployOptions: {
+				stageName: "dev",
+			},
+			defaultCorsPreflightOptions: {
+				allowHeaders: ["Content-Type", "X-Amz-Date"],
+				allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
+				allowCredentials: true,
+				allowOrigins: ["*"],
+			},
+		});
+
+		//Endpoints
+		const albumsEndpoint = api.root.addResource("albums");
+		albumsEndpoint.addMethod(
+			"GET",
+			new apig.LambdaIntegration(getAllAlbumsFn, {proxy: true})
+		);
+
+		const albumEndpoint = albumsEndpoint.addResource("{albumId}");
+		albumEndpoint.addMethod(
+			"GET",
+			new apig.LambdaIntegration(getAlbumByIdFn, {proxy: true})
+		);
+	}
 }
---
+++ seed/albums.ts
@@ -0,0 +1,100 @@
+import { Album } from "../shared/types"
+
+export const albums : Album[] = [
+    {
+        id: 1000,
+        artist: "The Joy Formidable",
+        title: "The Big Roar",
+        genres: ["Alt rock", "Indie rock", "Shoegaze"],
+        release_date: "24-01-2011",
+        review: "The Welsh band with Britpop-sized ambition offers its long-awaited full-length and it includes re-worked tracks from last year's mini album."
+    },
+    {
+        id: 1001,
+        artist: "My Chemical Romance",
+        title: "Three Cheers for Sweet Revenge",
+        genres: ["Emo", "Alt rock", "Pop-punk", "Post-hardcore", "Punk rock"],
+        release_date: "08-06-2004",
+        review: "Each Sunday, Pitchfork takes an in-depth look at a significant album from the past, and any record not in our archives is eligible. Today, we revisit My Chemical Romance's second album, an operatic pop-rock behemoth that became an icon for outcasts."
+    },
+    {
+        id: 1002,
+        artist: "my bloody valentine",
+        title: "loveless",
+        genres: ["Shoegaze", "Noise pop", "Dream pop", "Noise rock"],
+        release_date: "04-11-1991",
+        review: "My Bloody Valentine's output during the band's miracle years between 1988 and 1991 still feels like a gift."
+    },
+    {
+        id: 1003,
+        artist: "Wolf Alice",
+        title: "My Love Is Cool",
+        genres: ["Alt rock", "Dream pop", "Shoegaze"],
+        release_date: "22-06-2015",
+        review: "Wolf Alice have gained attention for their powerful live show and four EPs. Their debut is a tentative coming-of-age story, as guitarist/singer Ellie Rowsell refuses to settle for a single identity."
+    },
+    {
+        id: 1004,
+        artist: "Wolf Alice",
+        title: "Visions of a Life",
+        genres: ["Alt rock", "Noise rock"],
+        release_date: "29-09-2017",
+        review: "Wolf Alice proudly carry the banner for Britrock on their second album, a holy site where dead metaphors and teen clichés can spring magically back to life."
+    },
+    {
+        id: 1005,
+        artist: "Wolf Alice",
+        title: "Blue Weekend",
+        genres: ["Alt rock", "Rock", "Indie pop", "Shoegaze"],
+        release_date: "04-06-2021",
+        review: "The UK band's enormous third album is pristine and emotionally extravagant, the platonic ideal for contemporary big-tent rock music."
+    },
+    {
+        id: 1006,
+        artist: "underscores",
+        title: "Wallsocket",
+        genres: ["Pop", "Rock", "Folk"],
+        release_date: "22-09-2023",
+        review: "For the most part, 'Wallsocket' is the sound of an artist operating entirely, brilliantly on their own terms."
+    },
+    {
+        id: 1007,
+        artist: "Nine Inch Nails",
+        title: "Pretty Hate Machine",
+        genres: ["Industrial rock", "Electro-industrial", "Synth-pop"],
+        release_date: "20-10-1989",
+        review: "The landmark debut by Trent Reznor's band is reissued, with new mastering overseen by Reznor and longtime engineer Tom Baker and an additional B-side."
+    },
+    {
+        id: 1008,
+        artist: "Nine Inch Nails",
+        title: "The Downward Spiral",
+        genres: ["Industrial rock", "Alt rock", "Industrial metal"],
+        release_date: "08-03-1994",
+        review: "The Downward Spiral serves as a reminder that music can often be the purest form of human expression."
+    },
+    {
+        id: 1009,
+        artist: "IDLES",
+        title: "Joy as an Act of Resistance",
+        genres: ["Punk rock", "Post-punk", "Post-hardcore"],
+        release_date: "31-08-2018",
+        review: "The riffs come hard, fuzzy, and fast on the Bristol punks' deeply passionate second album—and the platitudes follow close behind."
+    },
+    {
+        id: 1010,
+        artist: "Jane Remover",
+        title: "Frailty",
+        genres: ["Digicore", "Hyperpop", "Emo", "EDM"],
+        release_date: "12-11-2021",
+        review: "The official debut from the young digicore producer and songwriter is riveting, a shapeshifting emo-electronic record that sounds like new worlds seeping out of a digital abyss."
+    },
+    {
+        id: 1011,
+        artist: "Jane Remover",
+        title: "Census Designated",
+        genres: ["Shoegaze", "Pop", "Noise rock"],
+        release_date: "20-10-2023",
+        review: "The singer-producer explodes her sound with a feverish blend of shoegaze and bedroom pop."
+    },
+]
